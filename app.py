883ed299405500031ff8810f2764168beb7c5322
AakBak <aak.bakkour@gmail.com>
2025-06-02 14:09:17 +0200

    Files

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="Distributed AI Architectures", layout="wide")
+
+with open("DAI.html", "r", encoding="utf-8") as f:
+    html = f.read()
+
+components.html(html, height=1800)
